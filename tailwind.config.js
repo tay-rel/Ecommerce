78aea9b4df248530405e4f45afdf91b3184c2e7b
tay-rel <sirenita0197@gmail.com>
2023-01-17 20:48:10 +0100

    Layout punto 13

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     mode: 'jit',
@@ -14,8 +15,17 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                trueGray: colors.trueGray,
+                orange: colors.orange,
+                lime: colors.lime,
+            }
         },
     },
-
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };

058f90049f9ac603b4fc4ef3bf5c71335e257756
tay-rel <sirenita0197@gmail.com>
2023-01-20 20:32:05 +0100

    Paso 36 slider en pagina principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,15 +15,45 @@
     <script>
         Livewire.on('glider', function (id) {
         new Glider(document.querySelector('.glider-' + id), {
-            slidesToShow: 5.5,
-            slidesToScroll: 5,
+            slidesToShow: 1,//5.5
+            slidesToScroll: 1,//5
             draggable: true,
             dots: '.glider-' + id + '~.dots',
             arrows: {
                 prev: '.glider-' + id + '~.glider-prev',
                 next: '.glider-' + id + '~.glider-next'
-            }
-        });
+            },
+            responsive: [
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 2.5,
+                        slidesToScroll: 2,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3.5,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4.5,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 1280,
+                    settings: {
+                        slidesToShow: 5.5,
+                        slidesToScroll: 5,
+                    }
+                }
+                ]
+            });
         });
     </script>
     @endpush

01fe8178f213177eb8df0a6e77b0b2439a5ec929
tay-rel <sirenita0197@gmail.com>
2023-01-31 19:20:55 +0100

    Paso 75 carrito de compra(habilitar botones)

--- app/Http/Livewire/UpdateCartItem.php
+++ app/Http/Livewire/UpdateCartItem.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Livewire;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 
 class UpdateCartItem extends Component
 {
     public $rowId;
+    public $qty, $quantity;
+    public function mount()
+    {
+        //en item quiero recuperar el producto
+        $item = Cart::get($this->rowId);
+        $this->qty = $item->qty;
+        $this->quantity = qty_available($item->id);
+    }
+    public function decrement()
+    {
+        $this->qty--;
+        //actualiza uno de sus items
+        Cart::update($this->rowId, $this->qty);
+        //el componente lo escucha dropdown
+        $this->emit('render');
+    }
+    public function increment()
+    {
+        $this->qty++;
+        Cart::update($this->rowId, $this->qty);
+        $this->emit('render');
+    }
     public function render()
     {
         return view('livewire.update-cart-item');

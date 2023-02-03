a68d89ce61b2c3d34298ded4d110debd5bd9a495
tay-rel <sirenita0197@gmail.com>
2023-02-03 19:30:18 +0100

    Paso 88 crear pedido primera parte(crear relaciones)

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,10 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-
+    //define que campos no quiero que se quede con la asignacion masiva
+    protected $guarded = ['id', 'created_at', 'updated_at', 'status'];
     const PENDIENTE = 1;
     const RECIBIDO = 2;
     const ENVIADO = 3;
     const ENTREGADO = 4;
     const ANULADO = 5;
+
+    //relacion uno a mucho inversa
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }

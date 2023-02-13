b58a28010686052910e2814f2b62b4359b2df0ed
tay-rel <sirenita0197@gmail.com>
2023-02-13 18:49:49 +0100

    Paso 94 , crear pedidos 3 parte(Crear ordenes la validacion no pasa si hace envio a domicilio)

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -35,6 +35,25 @@ class CreateOrder extends Component
     {
         $this->departments = Department::all();
     }
+
+    /* Si el usuario elige la opción de Envío a domicilio y presiona el botón
+sin rellenar los campos específicos, la validación no pasa*/
+
+    public function updatedEnvioType($value)
+    {
+        //$envio_type y si su valor
+        //es 1 (si cambia es porque era 2) entonces eliminamos los errores de validación de los campos:
+        if ($value == 1) {
+            $this->resetValidation([
+                'department_id',
+                'city_id',
+                'district_id',
+                'address',
+                'reference',
+            ]);
+        }
+    }
+
     public function render()
     {
         return view('livewire.create-order');

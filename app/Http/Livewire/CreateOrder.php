9f43964e80b3056ddda9b73942b5e4472cb7cbff
tay-rel <sirenita0197@gmail.com>
2023-02-13 19:32:48 +0100

    Paso 96 , crear pedidos 3 parte(Crear ordenes la validacion no pasa si hace envio a domicilio)

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\City;
 use App\Models\Department;
+use App\Models\District;
 use App\Models\Order;
 use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
@@ -43,6 +45,16 @@ class CreateOrder extends Component
         $order->shipping_cost = 0;
         $order->total = $this->shipping_cost + Cart::subtotal();
         $order->content = Cart::content();
+
+        if ($this->envio_type == 2) {
+            $order->shipping_cost = $this->shipping_cost;
+            $order->department_id = $this->department_id;
+            $order->city_id = $this->city_id;
+            $order->district_id = $this->district_id;
+            $order->address = $this->address;
+            $order->reference = $this->reference;
+        }
+
         $order->save();//se crea un nuevo registro
 
         Cart::destroy();
@@ -66,7 +78,21 @@ class CreateOrder extends Component
         }
     }
 
+    // Al seleccionar una ciudad se deben cargar
+    //los distritos asociados.
+    public function updatedDepartmentId($value){
+        $this->cities = City::where('department_id', $value)->get();
+        $this->reset(['city_id',
+            'district_id'
+        ]);
+    }
+    public function updatedCityId($value){
+        $city = City::find($value);
+        $this->shipping_cost = $city->cost;
 
+        $this->districts = District::where('city_id', $value)->get();
+        $this->reset('district_id');
+    }
 
     public function render()
     {

712b1a7c67f3aacf937d7cf23386da8ccae527ee
tay-rel <sirenita0197@gmail.com>
2023-02-16 17:13:30 +0100

    Paso 110 paypal (vista de pago ve los detalles caracteristicas)

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,14 +8,10 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
 
-    public function payment(Order $order)
+    public function show(Order $order)
     {
         //decodificque algo que este
         $items = json_decode($order->content);
-        return view('orders.payment', compact('order', 'items'));
-    }
-    public function show(Order $order)
-    {
-        return view('orders.show', compact('order'));
+        return view('orders.show', compact('order','items'));
     }
 }

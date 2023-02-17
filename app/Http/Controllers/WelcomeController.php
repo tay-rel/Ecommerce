f6b09ba9522e3d9294a2c907f98d5c15c7d710a1
tay-rel <sirenita0197@gmail.com>
2023-02-17 11:14:54 +0100

    Paso 116 gestionar pedidos(genera un mensaje flash si tienes un pedido pendiente)

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -10,6 +11,16 @@ class WelcomeController extends Controller
 
     public function __invoke()
     {
+        //en caso de que tenga pedidos pendientes de pago, le aparezca un banner recordándoselo.
+        if (auth()->user()) {
+            $pendientes = Order::where('user_id', auth()->user()->id)->where('status', 1)->count();
+            if ($pendientes) {
+                $mensaje = "Tiene $pendientes ordenes pendientes de pago. <a class='font-bold' href='" . route('orders.index') .
+                    "?status=1'>Pagar</a>";
+                session()->flash('flash.banner', $mensaje);
+                session()->flash('flash.bannerStyle', 'danger');
+            }
+        }
 
         $categories = Category::all();
         return view('welcome', compact('categories'));

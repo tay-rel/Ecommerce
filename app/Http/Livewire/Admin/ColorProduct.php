5bae7e57dbad8b4cd295afb583facfc813568c8d
tay-rel <sirenita0197@gmail.com>
2023-02-18 23:15:06 +0100

    Paso 136 edicion de productos(agrega color y talla en la bbdd , pero se deb corregir error)

--- app/Http/Livewire/Admin/ColorProduct.php
+++ app/Http/Livewire/Admin/ColorProduct.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Color;
 use Livewire\Component;
 
 class ColorProduct extends Component
 {
-    public function render()
+    protected $rules = [
+        'color_id' => 'required',
+        'quantity' => 'required|numeric'
+    ];
+    public $product, $colors;
+    public $color_id, $quantity;
+    public function mount()
+    {
+        $this->colors = Color::all();
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->product->colors()->attach([$this->color_id => ['quantity' => $this->quantity]]);
+        $this->reset(['color_id', 'quantity']);
+        $this->emit('saved');
+
+    }
+
+        public function render()
     {
         return view('livewire.admin.color-product');
     }

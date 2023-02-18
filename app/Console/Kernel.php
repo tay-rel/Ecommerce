5800a7686096cd04e9bcb975154674cc9bbb76fb
tay-rel <sirenita0197@gmail.com>
2023-02-18 16:22:59 +0100

    Paso 119 gestionar pedidos (duración de pedidos y su stock)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\Order;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,33 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+//        //horario =programar una tarea para que se ejecuta en un cierto tiempo
+//        $schedule->call(function () {
+//            $orders = Order::where('status', 1)->where('created_at','<',now()->subMinutes(10))->get();
+//            //itera cada una de las ordenes
+//            foreach ($orders as $order) {
+//                //donde se enceuntra el lstado de productoos en formato string
+//                $items = json_decode($order->content);
+//                //recorre el listado y cada uno de los elementos que contiene
+//                foreach ($items as $item) {
+//                    increase($item);//incrementa el stock en la misma cantidad que tenia el producto que esta en el helper
+//                }
+//                $order->status = 5;
+//                $order->save();
+//            }
+//        })->everyMinute();
+
+        $schedule->call(function () {
+            $orders = Order::where('status', 1)->where('created_at','<',now()->subMinutes(10))->get();
+            foreach ($orders as $order) {
+                $items = json_decode($order->content);
+                foreach ($items as $item) {
+                    increase($item);
+                }
+                $order->status = 5;
+                $order->save();
+            }
+        })->everyMinute();
     }
 
     /**

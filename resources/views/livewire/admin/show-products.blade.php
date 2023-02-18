1fdbb27158b764ccd0cb75e05062d698610bee94
tay-rel <sirenita0197@gmail.com>
2023-02-18 17:23:20 +0100

    Paso 122 creación de productos (vista productos)

--- resources/views/livewire/admin/show-products.blade.php
+++ resources/views/livewire/admin/show-products.blade.php
@@ -4,4 +4,61 @@
             Lista de productos
         </h2>
     </x-slot>
-</div
+<!--Buscador-->
+    <div class="px-6 py-4">
+        <x-jet-input class="w-full"
+                     wire:model="search"
+                     type="text"
+                     placeholder="Introduzca el nombre del producto a buscar" />
+    </div>
+
+    <x-table-responsive>
+        <table class="min-w-full divide-y divide-gray-300">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
+                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                            @foreach($products as $product)
+                            <tr>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                                    <div class="flex items-center">
+                                        <div class="h-10 w-10 flex-shrink-0">
+                                            <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                        </div>
+                                        <div class="ml-4">
+                                            <div class="font-medium text-gray-900">Lindsay Walton</div>
+                                            <div class="text-gray-500">lindsay.walton@example.com</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <div class="text-gray-900">Front-end Developer</div>
+                                    <div class="text-gray-500">Optimization</div>
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">Active</span>
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
+                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                </td>
+                            </tr>
+                            @endforeach
+                            <!-- More people... -->
+                            </tbody>
+                        </table>
+                    <!--Paginacion-->
+                        <div class="px-6 py-4">
+                            {{ $products->links() }}
+                        </div>
+    </x-table-responsive>
+
+</div>

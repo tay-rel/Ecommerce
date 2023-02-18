683776dd84f473cf29552b9aa4bb95b3db759823
tay-rel <sirenita0197@gmail.com>
2023-02-18 17:31:29 +0100

    Paso 123 creación de productos (vista prodcutos nombres, precios y categorias)

--- routes/admin.php
+++ routes/admin.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', ShowProducts::class)->name('admin.index');
+Route::get('products/{product}/edit', function () {})->name('admin.products.edit');

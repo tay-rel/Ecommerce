0a41c1b1a5f15802cbda689f837ba8adc68bc20f
tay-rel <sirenita0197@gmail.com>
2023-02-20 20:45:21 +0100

    Paso 159 gestion de categorias (categorias y reglas de validacion)

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -3,11 +3,34 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Brand;
+use Illuminate\Support\Str;
 use Livewire\Component;
-
+use WithFileUploads;
 class CreateCategory extends Component
 {
     public $brands;
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required',
+    ];
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+    ];
+
     public function mount()
     {
         $this->getBrands();
@@ -18,6 +41,11 @@ class CreateCategory extends Component
     }
     public function save()
     {
+        $this->validate();
+    }
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
     }
     public function render()
     {
--- resources/views/livewire/admin/create-category.blade.php
+++ resources/views/livewire/admin/create-category.blade.php
@@ -11,19 +11,22 @@
                 <x-jet-label>
                     Nombre
                 </x-jet-label>
-                <x-jet-input type="text" class="w-full mt-1" />
+                <x-jet-input wire:model="createForm.name" type="text" class="w-full mt-1" />
+                <x-jet-input-error for="createForm.name" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-jet-label>
                     Slug
                 </x-jet-label>
-                <x-jet-input type="text" class="w-full mt-1" />
+                <x-jet-input wire:model="createForm.slug" type="text" class="w-full mt-1" />
+                <x-jet-input-error for="createForm.slug" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-jet-label>
                     Icono
                 </x-jet-label>
-                <x-jet-input type="text" class="w-full mt-1" />
+                <x-jet-input wire:model.defer="createForm.icon" type="text" class="w-full mt-1" />
+                <x-jet-input-error for="createForm.icon" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-jet-label>
@@ -32,20 +35,27 @@
                 <div class="grid grid-cols-4">
                     @foreach ($brands as $brand)
                         <x-jet-label>
-                            <x-jet-checkbox />
+                            <x-jet-checkbox wire:model.defer="createForm.brands"
+                                            name="brands[]"
+                                            value="{{$brand->id}}"  />
                             {{$brand->name}}
                         </x-jet-label>
                     @endforeach
                 </div>
+                <x-jet-input-error for="createForm.brands" />
             </div>
             <div class="col-span-6 sm:col-span-4">
                 <x-jet-label>
                     Imagen
                 </x-jet-label>
-                <input type="file" class="mt-1" name="" id="">
+                <input wire:model="createForm.image" accept="image/*" type="file" class="mt-1" name="" id="">
+                <x-jet-input-error for="createForm.image" />
             </div>
         </x-slot>
         <x-slot name="actions">
+            <x-jet-button>
+                Agregar
+            </x-jet-button>
         </x-slot>
     </x-jet-form-section>
 </div>

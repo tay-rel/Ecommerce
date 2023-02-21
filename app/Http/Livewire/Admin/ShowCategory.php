0eb7e981cd3ebe756af569647df2b264f0f36d07
tay-rel <sirenita0197@gmail.com>
2023-02-22 00:22:56 +0100

    Paso 169 gestion de subcategorias (creacion de formulario y validacion de envio)

--- app/Http/Livewire/Admin/ShowCategory.php
+++ app/Http/Livewire/Admin/ShowCategory.php
@@ -3,14 +3,51 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Category;
+use App\Models\Subcategory;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class ShowCategory extends Component
 {
-    public $category;
+    public $category, $subcategories;
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'color' => false,
+        'size' => false,
+    ];
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:subcategories,slug',
+        'createForm.color' => 'required',
+        'createForm.size' => 'required',
+    ];
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.color' => 'color',
+        'createForm.size' => 'talla',
+    ];
     public function mount(Category $category)
     {
         $this->category = $category;
+        $this->getSubcategories();
+    }
+    public function getSubcategories()
+    {
+        $this->subcategories = Subcategory::where('category_id', $this->category->id)->get();
+    }
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
+    public function save()
+    {
+        $this->validate();
+    }
+    public function edit(Subcategory $subcategory)
+    {
     }
     public function render()
     {

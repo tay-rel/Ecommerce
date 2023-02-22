2141ca7f2e21cecbeb82a57c40d1d766128f911d
tay-rel <sirenita0197@gmail.com>
2023-02-22 11:14:26 +0100

    Paso 174 gestion de departamentos (vista , formulario de departamento)

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -25,6 +25,9 @@
                     <x-jet-nav-link href="{{ route('admin.orders.index') }}" :active="request()->routeIs('admin.orders.*')">
                         Pedidos
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('admin.departments.index') }}" :active="request()->routeIs('admin.departments.*')">
+                        Departamentos
+                    </x-jet-nav-link>
                 </div>
             </div>
 
@@ -160,6 +163,9 @@
             </x-jet-responsive-nav-link>
             <x-jet-responsive-nav-link href="{{ route('admin.orders.index') }}" :active="request()->routeIs('admin.orders.*')">
                 Pedidos
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('admin.departments.index') }}" :active="request()->routeIs('admin.departments.*')">
+                Departamentos
             </x-jet-responsive-nav-link>
         </div>
 
--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Livewire\Admin\BrandComponent;
 use App\Http\Livewire\Admin\CreateProduct;
+use App\Http\Livewire\Admin\DepartmentComponent;
 use App\Http\Livewire\Admin\EditProduct;
 use App\Http\Livewire\Admin\ShowCategory;
 use App\Http\Livewire\Admin\ShowProducts;
@@ -27,3 +28,6 @@ Route::get('brands', BrandComponent::class)->name('admin.brands.index');
 
 Route::get('orders', [OrderController::class, 'index'])->name('admin.orders.index');
 Route::get('orders/{order}', [OrderController::class, 'show'])->name('admin.orders.show');
+
+Route::get('departments', DepartmentComponent::class)->name('admin.departments.index');
+

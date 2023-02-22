2cb67838098b68dc1661ddaa3acf0a979c72e1e5
tay-rel <sirenita0197@gmail.com>
2023-02-22 12:42:09 +0100

    Paso 183 laravel permission (crud usuario, listado de usuarios)

--- app/Http/Livewire/Admin/UserComponent.php
+++ app/Http/Livewire/Admin/UserComponent.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
 
 class UserComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.user-component')->layout('layouts.admin');
+        $users = User::paginate();
+        return view('livewire.admin.user-component', compact('users'))->layout('layouts.admin');
     }
 }

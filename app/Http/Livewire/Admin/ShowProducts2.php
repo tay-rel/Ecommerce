95365f3d99e7a5cd971432ef8f2dfde88174180a
tay-rel <sirenita0197@gmail.com>
2023-02-26 19:48:53 +0100

    ejercicio 2 proceso

--- app/Http/Livewire/Admin/ShowProducts2.php
+++ app/Http/Livewire/Admin/ShowProducts2.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ShowProducts2 extends Component
 {
+    public $search;
+
+    public function updatingSearch()
+    {//cada vez que se busque se retorna a la pagina uno
+        $this->resetPage();
+    }
     public function render()
     {
-        return view('livewire.admin.show-products2');
+        $products = Product::query()
+            ->applyFilters([
+                'search'=>$this->search
+            ])->paginate(10);
+        return view('livewire.admin.show-products2', compact('products')) ->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/show-products2.blade.php
+++ resources/views/livewire/admin/show-products2.blade.php
@@ -1,3 +1,82 @@
 <div>
-  Estoy dentro de mostrar productos 2
+    <x-slot name="header">
+        <div class="flex items-center">
+            <h2 class="font-semibold text-xl text-gray-600 leading-tight">
+                Lista de productos 2
+            </h2>
+        </div>
+    </x-slot>
+
+
+    <x-table-responsive>
+        <!--Buscador-->
+
+        <div class="px-6 py-4">
+            <x-jet-input class="w-full"
+                         wire:model="search"
+                         type="text"
+                         placeholder="Introduzca el nombre del producto a buscar" />
+        </div>
+        @if($products->count())
+            <table class="min-w-full divide-y divide-gray-300">
+                <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Nombre</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Categoría</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Estado</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Precio</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Marca</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Productos vendidos</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Stock disponible</th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"> Fecha creación del producto</th>
+
+                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                        <span class="sr-only">Editar</span>
+                    </th>
+
+                </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                @foreach($products as $product)
+                    <tr>
+                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                            <div class="flex items-center">
+                                <div class="h-10 w-10 flex-shrink-0 object-cover">
+                                    <img class="h-10 w-10 rounded-full" src="{{  $product->images->count() ? Storage::url($product->images->first()->url) :
+'img/default.jpg' }}" alt="">
+                                </div>
+                                <div class="ml-4">
+                                    <div class="font-medium text-gray-900">{{ $product->name }}</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            <div class="text-gray-900">{{ $product->subcategory->category->name }}</div>
+                            <div class="text-gray-500">{{ $product->subcategory->name }}</div>
+                        </td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <span class="inline-flex rounded-full bg-{{ $product->status == 1 ? 'red' : 'green'
+                                        }}-100 px-2 text-xs font-semibold leading-5 text-{{ $product->status == 1 ? 'red' : 'green' }}-800">{{ $product->status == 1 ? 'Borrador' : 'Publicado' }}</span>
+                        </td>
+                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $product->price }} &euro;</td>
+                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                            <a href="{{ route('admin.products.edit', $product) }}" class="text-indigo-600 hover:text-indigo-900">Editar</a>
+                        </td>
+                    </tr>
+                @endforeach
+                <!-- More people... -->
+                </tbody>
+            </table>
+        @else
+            <div class="px-6 py-4">
+                No existen productos coincidentes
+            </div>
+        @endif
+        <!--Paginacion-->
+        @if($products->hasPages())
+            <div class="px-6 py-4">
+                {{ $products->links() }}
+            </div>
+        @endif
+    </x-table-responsive>
 </div>
